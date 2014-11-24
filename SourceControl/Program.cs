b2564b35b2500975af035194cc2334fb0adcc870
Mathias <mathias-poulsen@hotmail.com>
2014-11-24 13:53:27 +0100

    test 2

--- SourceControl/Program.cs
+++ SourceControl/Program.cs
@@ -11,6 +11,7 @@ namespace SourceControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Mere hello world!");
             Console.ReadKey();
         }
     }
